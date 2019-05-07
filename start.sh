88111cb0cdbf3aff4b7ded5821d37fe680b48272
gitfreebeer <gitfreebeer@users.noreply.github.com>
2019-05-07 12:35:48 -0700

    Updated subnet
    Changed subnet for bridge from 172.21.1.1/24 to 192.168.251.1/24 and updated DHCP scope. Also changed to 1h lease times.

--- start.sh
+++ start.sh
@@ -10,11 +10,11 @@ fi
 brctl addbr virbr0
 ip link set dev virbr0 up
 if [ "${BRIDGE_ADDRESS}x" == "x" ]; then
-  BRIDGE_ADDRESS=172.21.1.1/24
+  BRIDGE_ADDRESS=192.168.251.1/24
 fi
 ip ad add ${BRIDGE_ADDRESS} dev virbr0
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
-dnsmasq -i virbr0 -z -h --dhcp-range=172.21.1.10,172.21.1.250,4h
+dnsmasq -i virbr0 -z -h --dhcp-range=192.168.251.10,192.168.251.250,1h
 dockerd --storage-driver=vfs --data-root=/data/docker/ &
 gns3server -A --config /data/config.ini
